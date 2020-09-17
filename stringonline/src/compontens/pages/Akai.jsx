f521f4b3ab6ef414bdadb5b00f85659c7dc0d965
kriser21 <kristina@nebe.dk>
2020-09-17 12:55:01 +0200

    nyt

--- stringonline/src/compontens/pages/Akai.jsx
+++ stringonline/src/compontens/pages/Akai.jsx
@@ -1,10 +1,58 @@
+import React, { useState, useEffect } from 'react';
 
-import React from 'react'
+const Fetch = () =>{
+const [apiData, setApiData]= useState (null);
+useEffect(() => {
+if (!apiData){
 
-export default function Akai() {
-    return (
-        <div>
+
+fetch('https://api.mediehuset.net/stringsonline/brands/17', {
+method:'GET',
+redirect: 'follow',
+})
             
-        </div>
-    )
+.then((res) => res.json())
+.then((data) => setApiData(data))
+.catch((err) => console.log(err));        
+}
+});
+
+console.log(apiData && apiData);
+
+let home =
+apiData&&
+    apiData.item.products.slice(0, 3).map((news) =>{
+        console.log (news);
+        return(
+            <p>
+            <img src={news.image_fullpath} alt="" />
+            <span>
+            <p>{news.name}</p>
+        <p>{news.price}</p>
+          </span>
+            </p>
+         );
+    });
+
+
+
+return ( <div className="Text"> 
+   <div className="HomeHotel">
+<h2>{apiData && apiData.item.title}</h2>
+<p>{apiData && apiData.item.description}</p>
+<img src={apiData && apiData.item.image_fullpath} alt=""/>
+
+  {home}
+   </div> 
+    </div>
+    );
+};
+
+export default function Home(){
+    return(
+        <section>
+        
+            <Fetch/>
+        </section>
+    );
 }
